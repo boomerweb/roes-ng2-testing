2e8bbf14ad1faaf34431664acd91ffee237f8838
Michael Roes <mroes88@gmail.com>
2017-03-10 14:15:13 +0100

    blah blah

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,7 @@
 import { Component } from '@angular/core';
-
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  moduleId: module.id,
+  selector: 'my-app',
+  templateUrl: './app.component.html'
 })
-export class AppComponent {
-  title = 'app works!';
-}
+export class AppComponent { }
